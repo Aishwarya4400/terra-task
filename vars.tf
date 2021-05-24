ade2b6f8f367a7624ed5a74ab088a047048a93eb
Ubuntu <ubuntu@ip-10-0-1-217.ap-south-1.compute.internal>
2021-05-24 13:41:48 +0000

    first

---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+ default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+ default = "mykey.pub"
+}
+variable "AMIS" {
+ type = map(string)
+ default = {
+ ap-south-1 = "ami-04bde106886a53080"
+ }
+}
+
+
+
